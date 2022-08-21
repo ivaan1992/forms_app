4e20d3fee79401dc73b434216557bb605f1ea32b
Ivan Gaona <ivaan.lg.92@gmail.com>
2022-08-20 23:12:21 -0500

    Create modules and sidebar component

---
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidemenu',
+  templateUrl: './sidemenu.component.html',
+  styleUrls: []
+})
+export class SidemenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
