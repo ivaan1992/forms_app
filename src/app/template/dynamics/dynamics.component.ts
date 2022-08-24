03eff9e61b9c0c72640e1568a1cd9c16b6bf36ff
Ivan Gaona <ivaan.lg.92@gmail.com>
2022-08-24 11:49:04 -0500

    Create interfaces, add games to the form, add delete game method

--- src/app/template/dynamics/dynamics.component.ts
+++ src/app/template/dynamics/dynamics.component.ts
@@ -1,19 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+
+interface Person {
+  name: string;
+  favorites: Favorite[];
+}
+
+interface Favorite {
+  id: number;
+  game_name: string;
+}
 
 @Component({
   selector: 'app-dynamics',
   templateUrl: './dynamics.component.html',
   styleUrls: []
 })
-export class DynamicsComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+export class DynamicsComponent {
+
+  person: Person = {
+    name: 'Ivan',
+    favorites: [
+      { id: 1, game_name: 'Mario Kart' },
+      { id: 2, game_name: 'King Of Fighter' },
+      { id: 3, game_name: 'Mortal Kombat' },
+    ]
   }
 
   save() {
     console.log('Form posted!!')
+  }
+
+  deleteGame( index: number ) {
+    this.person.favorites.splice( index, 1 );
   }
 
 }
