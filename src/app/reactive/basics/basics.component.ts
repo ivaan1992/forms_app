d61d9fb3749dffb1f3e2a6ed922f52dcad0c6f7c
Ivan Gaona <ivaan.lg.92@gmail.com>
2022-08-25 22:28:14 -0500

    Finish the basic reactive forms implementing formbuilder, validators, submit functions & showing error messages

--- src/app/reactive/basics/basics.component.ts
+++ src/app/reactive/basics/basics.component.ts
@@ -1,17 +1,45 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit  } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basics',
   templateUrl: './basics.component.html',
   styleUrls: []
 })
-export class BasicsComponent {
-  myForm: FormGroup = new FormGroup({
-    'name': new FormControl('Iphone 13 Pro Max')
+export class BasicsComponent implements OnInit {
+
+  myForm: FormGroup = this.fb.group({
+    name: [ '', [ Validators.required, Validators.minLength(3) ] ],
+    price: [ 0, [ Validators.required, Validators.min(1) ] ],
+    stocks: [ 0, [ Validators.required, Validators.min(1) ] ],
   })
 
+  ngOnInit() {
+    this.myForm.reset({
+      name: 'Product Name',
+      price: 1200
+    })
+  }
 
-  constructor() { }
+  constructor( private fb: FormBuilder ) { }
+
+  showErrorMessage( input: string ) {
+    return this.myForm.controls[input].errors
+           &&
+           this.myForm.controls[input].touched
+  }
+
+  saveFormInfo() {
+
+    if( this.myForm.invalid ) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log( this.myForm.value );
+
+    this.myForm.reset();
+
+  }
 
 }
