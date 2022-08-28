767ab7437f3cfdf2ef1933ff88d1107951a4f597
Ivan Gaona <ivaan.lg.92@gmail.com>
2022-08-28 14:54:16 -0500

    Finish dynamics forms reactives

--- src/app/reactive/dynamics/dynamics.component.ts
+++ src/app/reactive/dynamics/dynamics.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamics',
@@ -9,7 +9,19 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class DynamicsComponent {
   gameForm: FormGroup = this.fb.group({
     user: ['', [ Validators.required, Validators.minLength(3) ] ],
+    list: this.fb.array([
+      ['Mortal Kombat', Validators.required],
+      ['Mario', Validators.required],
+      ['King Of Fighters', Validators.required],
+    ], Validators.required  )
+
   })
+
+  newGame: FormControl = new FormControl
+
+  get listArr() {
+    return this.gameForm.get('list') as FormArray;
+  }
 
   constructor( private fb: FormBuilder ) { }
 
@@ -17,6 +29,20 @@ export class DynamicsComponent {
     return this.gameForm.controls[input].errors
            &&
            this.gameForm.controls[input].touched
+  }
+
+  addGame() {
+    if( this.newGame.invalid ) {
+      return;
+    }
+
+    this.listArr.push( new FormControl( this.newGame.value, Validators.required ) );
+
+    this.newGame.reset();
+  }
+
+  deleteGame( i: number ) {
+    this.listArr.removeAt(i);
   }
 
   saveFormInfo() {
